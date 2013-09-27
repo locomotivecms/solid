8240bce5cde0d66ae0cb0221b19113a5116f7770
S. Tunji Turner <vision@firstsightmedia.com>
2013-09-27 17:57:40 -0400

    add scope to interpolate method

--- lib/solid/block.rb
+++ lib/solid/block.rb
@@ -1,7 +1,8 @@
 class Solid::Block < Liquid::Block
 
   include Solid::Element
-
+  include Solid::Arguments
+  
   def render(context)
     with_context(context) do
       display(*arguments.interpolate(context)) do
